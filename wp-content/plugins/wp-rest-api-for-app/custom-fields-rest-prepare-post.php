b4f34bde871b6081252f8d6868bb24401d0feb56
iat <i@iat.net.cn>
2018-01-25 09:04:15 +0800

    增加rest api

---
+++ wp-content/plugins/wp-rest-api-for-app/custom-fields-rest-prepare-post.php
@@ -0,0 +1,194 @@
+<?php 
+add_filter( 'rest_prepare_post', 'custom_fields_rest_prepare_post', 10, 3 ); //获取文章的缩略图，评论数目，分类名称
+
+//在rest api 增加显示字段
+function custom_fields_rest_prepare_post( $data, $post, $request) { 
+
+    global $wpdb;
+
+    $_data = $data->data;    
+    //$post_id = ( null === $post_id ) ? get_the_ID() : $post_id;
+    $post_id =$post->ID;
+    
+    $images =getPostImages(get_the_content(), $post_id); 
+    $_data['post_thumbnail_image']=$images['post_thumbnail_image'];
+    $_data['content_first_image']=$images['content_first_image'];
+    $_data['post_medium_image_300']=$images['post_medium_image_300'];
+    $_data['post_thumbnail_image_624']=$images['post_thumbnail_image_624'];
+    $comments_count = wp_count_comments($post_id);
+    
+    $pageviews = (int) get_post_meta( $post_id, 'wl_pageviews',true);
+    $_data['pageviews'] = $pageviews;
+    
+    $_data['total_comments']=$comments_count->total_comments;
+    $category =get_the_category($post_id);
+    $_data['category_name'] =$category[0]->cat_name; 
+    /*
+    $content  =get_the_content();    
+    $_content['rendered'] =$content;
+    $_data['content']= $_content; 
+    */
+
+    
+    $like_count = $wpdb->get_var("SELECT COUNT(1) FROM ".$wpdb->postmeta." where meta_value='like' and post_id=".$post_id);
+    $_data['like_count']= $like_count;
+    $params = $request->get_params();
+     if ( isset( $params['id'] ) ) { 
+            
+            $sql="SELECT meta_key , (SELECT display_name from ".$wpdb->users." WHERE user_login=substring(meta_key,2)) as avatarurl FROM ".$wpdb->postmeta." where meta_value='like' and post_id=".$post_id;
+            $likes = $wpdb->get_results($sql);
+            $avatarurls =array();
+            foreach ($likes as $like) {
+                $_avatarurl['avatarurl']  =$like->avatarurl;   
+                $avatarurls[] = $_avatarurl;        
+            }
+        }
+        else 
+        {
+            unset($_data['content'] );   
+            unset($_data['author']); 
+        }
+    
+    
+    $_data['avatarurls']= $avatarurls; 
+
+
+    $next_post = get_next_post($category_id, '', 'category');
+    $previous_post = get_previous_post($category_id, '', 'category');
+    $_data['next_post_id'] = $next_post->ID;
+    $_data['next_post_title'] = $next_post->post_title;
+    $_data['previous_post_id'] = $previous_post->ID;
+    $_data['previous_post_title'] = $previous_post->post_title;
+        
+        
+        
+    
+    //$unset( $_data['content'] );
+
+    unset($_data['excerpt']);
+    unset($_data['featured_media']);
+    unset($_data['format']);
+    unset($_data['ping_status']);
+    unset($_data['template']);
+    unset($_data['type']);
+    //unset($_data['slug']);
+    unset($_data['modified_gmt']);
+    unset($_data['date_gmt']);
+    unset($_data['meta']);
+    unset($_data['guid']);
+    unset($_data['curies']);
+    unset($_data['modified']);
+    unset($_data['status']);
+    unset($_data['comment_status']);
+    unset($_data['sticky']);
+    unset($_data['author']); 
+      
+    $data->data = $_data; 
+    
+    return $data; 
+}
+
+
+add_action( 'rest_api_init', function () {
+  register_rest_route( 'watch-life-net/v1', 'post/swipe', array(
+    'methods' => 'GET',
+    'callback' => 'getPostSwipe'
+  ) );
+} );
+
+
+function getPostSwipe($request) {    
+    
+    $data=post_swipe_json(); 
+    if (empty($data)) {
+        return new WP_Error( 'error', 'post swipe is  error', array( 'status' => 404 ) );
+      }
+     $response = new WP_REST_Response($data);
+     $response->set_status( 200 ); 
+     return $response;
+
+}
+
+function post_swipe_json(){
+        global $wpdb;
+        $postSwipeIDs = get_option('wf_swipe');
+		$posts =array();
+
+		$siteurl= site_url();
+		// $pos = strpos($siteurl, 'www.watch-life.net');
+
+		// if($pos)
+		// {
+		// 	$_data["id"]  ="1";
+	 //        $_data["post_title"] =""; 
+	 //        $_data["like_count"] ="";  
+	 //        $_data["post_date"] =""; 
+	 //        $_data["post_permalink"] ="";
+	 //        $_data['pageviews'] ="";
+	 //        $_data['post_thumbnail_image']="https://www.watch-life.net/images/weixinapp.jpg";
+	 //        $_data['content_first_image']="https://www.watch-life.net/images/weixinapp.jpg";
+	 //        $_data['post_medium_image_300']="https://www.watch-life.net/images/weixinapp.jpg";
+	 //        $_data['post_thumbnail_image_624']="https://www.watch-life.net/images/weixinapp.jpg";
+	 //        $_data['comment_total']="0"; 
+	 //        $_data['type']="apppage";                 
+	 //        $posts[] = $_data;  
+		// }
+
+		      
+        if(!empty($postSwipeIDs))
+        {
+            $sql="SELECT *  from ".$wpdb->posts." where id in(".$postSwipeIDs.")";
+            $_posts = $wpdb->get_results($sql);
+            
+            foreach ($_posts as $post) {
+    
+                $post_id = (int) $post->ID;
+                $post_title = stripslashes($post->post_title);
+                $like_total = (int) $post->like_total;
+                $post_date =$post->post_date;
+                $post_permalink = get_permalink($post->ID);            
+                $_data["id"]  =$post_id;
+                $_data["post_title"] =$post_title; 
+                $_data["like_count"] =$like_total;  
+                $_data["post_date"] =$post_date; 
+                $_data["post_permalink"] =$post_permalink;
+                $_data['type']="detailpage";  
+                
+                $pageviews = (int) get_post_meta( $post_id, 'wl_pageviews',true);
+                $_data['pageviews'] = $pageviews;
+
+                $comment_total = $wpdb->get_var("SELECT COUNT(1) FROM ".$wpdb->comments." where  comment_approved = '1' and comment_post_ID=".$post_id);
+                $_data['comment_total']= $comment_total;
+
+                $images =getPostImages($post->post_content,$post_id);         
+                
+                $_data['post_thumbnail_image']=$images['post_thumbnail_image'];
+                $_data['content_first_image']=$images['content_first_image'];
+                $_data['post_medium_image_300']=$images['post_medium_image_300'];
+                $_data['post_thumbnail_image_624']=$images['post_thumbnail_image_624'];
+                $posts[] = $_data;
+
+                
+            
+            
+            }
+
+            $result["code"]="success";
+            $result["message"]= "get post  swipe success  ";
+            $result["status"]="200";
+            $result["posts"]=$posts;      
+            return $result;
+  
+
+            
+        
+        }
+        else
+        {
+            $result["code"]="success";
+            $result["message"]= " get post swipe error";
+            $result["status"]="500";                   
+            return $result;
+        }
+     
+}
---
+++ wp-content/plugins/wp-rest-api-for-app/wp-rest-api-for-app.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: WP REST API For App
+Plugin URI: http://www.watch-life.net
+Description: 为微信小程序、app提供定制WordPress rest api
+Version: 1
+Author: jianbo
+Author URI: http://www.watch-life.net
+License: GPL v3
+*/
+
+
+define('WP_REST_API_FOR_APP_PLUGIN_DIR', plugin_dir_path(__FILE__));
+include(WP_REST_API_FOR_APP_PLUGIN_DIR . 'weixin-openid.php');    // 获取微信openid
+include(WP_REST_API_FOR_APP_PLUGIN_DIR . 'allow-anonymous-comments.php');    // 开启匿名评论
+include(WP_REST_API_FOR_APP_PLUGIN_DIR . 'category-cover.php');    // 设置分类的微信小程序封面
+include(WP_REST_API_FOR_APP_PLUGIN_DIR . 'top-hot-posts.php');    // 获取热门文章
+include(WP_REST_API_FOR_APP_PLUGIN_DIR . 'addpageview.php');    // 更新文章浏览数
+include(WP_REST_API_FOR_APP_PLUGIN_DIR . 'custom-fields-rest-prepare-post.php');    // 自定义文章输出的字段
+include(WP_REST_API_FOR_APP_PLUGIN_DIR . 'post-like.php');    // 点赞
+include(WP_REST_API_FOR_APP_PLUGIN_DIR . 'weixin-praise.php');    //赞赏 
+include(WP_REST_API_FOR_APP_PLUGIN_DIR . 'api.php');    // 公用函数
+include(WP_REST_API_FOR_APP_PLUGIN_DIR . 'weixin-comment.php');    // 微信提交评论
+include(WP_REST_API_FOR_APP_PLUGIN_DIR . 'weixin-send-message.php');    // 发送微信模版消息
+include(WP_REST_API_FOR_APP_PLUGIN_DIR . 'do-not-show-users.php');    // 不显示用户列表
+include(WP_REST_API_FOR_APP_PLUGIN_DIR . 'category-subscription.php');    // 分类订阅，显示，取消
+include(WP_REST_API_FOR_APP_PLUGIN_DIR . 'custom-fields-rest_prepare_comment.php');    // 自定义评论输出的字段
+
+
